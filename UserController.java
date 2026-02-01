84100b9e905a189047a51694a33941285ac3fc09
Talha Khan <talha4khan@hotmail.com>
2026-02-01 22:32:16 +0500

    Include server port in hello response

--- UserController.java
+++ UserController.java
@@ -2,12 +2,17 @@ package com.example.demo;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 
 @RestController
 public class UserController {
 
+     @Autowired
+    private Environment env;
+
     @GetMapping("/hello")
     public String hello() {
-        return "Hello World";
+        return "Hello World" + env.getProperty("local.server.port");
     }
 }
